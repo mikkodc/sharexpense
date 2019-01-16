c8081892fa61529467772019ea4be2dae8ad2208
Mikko - C2Native <ainia@c2native.io>
2019-01-16 08:50:45 +0400

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import firebase from 'firebase';
 import Vue from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -6,8 +8,23 @@ import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app = '';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB2pvfIfrZJ7eKOOPP7bDo4nbhka3lBPhg",
+  authDomain: "share-expense-c8519.firebaseapp.com",
+  databaseURL: "https://share-expense-c8519.firebaseio.com",
+  projectId: "share-expense-c8519",
+  storageBucket: "",
+  messagingSenderId: "261585604660"
+});
+
+firebase.auth().onAuthStateChanged(()=> {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
---
+++ src/views/SignUp.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <div class="sign-up">
+    <p>Let's create a new account!</p>
+    <input type="text" v-model="email" placeholder="Email">
+    <input type="password" v-model="password" placeholder="Password">
+    <button @click="signUp">Sign Up</button>
+    <span>or go back to <router-link to="/login">login</router-link></span>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: 'signUp',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signUp: function() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+        function(user) {
+          alert('Your account has been created!');
+          this.$router.push('/');
+        },
+        function(err) {
+          alert('Oops. ' + err.message)
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.sign-up {
+  margin-top: 40px;
+}
+input {
+  display: block;
+  margin: 10px auto;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 10px;
+  width: 10%;
+  cursor: pointer;
+}
+span {
+  display: block;
+  margin-top: 20px;
+  font-size: 11px;
+}
+</style>
